72983080c33fce5824a0fba9f41c47406de7f351
root <root@birch.>
2024-12-19 12:43:21 +0800

    feat(add): explanation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,8 +67,19 @@ const initialStates = [
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className="container flex flex-1 items-center justify-center px-10">
+      <div className="container flex flex-1 flex-col items-center justify-center gap-[5vw] px-10">
         <TabsExample initialStates={initialStates} />
+        <div>
+          <p className="break-words">Default tab is `tab_5`</p>
+          <p>
+            tab_5 status:{" "}
+            {JSON.stringify(
+              { notifications: "false", tabDisabled: "true" },
+              null,
+              2,
+            )}
+          </p>
+        </div>
       </div>
     </main>
   );
